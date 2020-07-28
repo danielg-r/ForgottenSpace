75f2fce2f491aeffffbb9c13545f64aaba631c72
yefko14 <61544277+yefko14@users.noreply.github.com>
2020-07-28 18:12:08 -0500

    Control Basico y Animaciones

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        anim.SetFloat("vertical", Input.GetAxis("Vertical"));
+        anim.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+
+
+    }
+}
