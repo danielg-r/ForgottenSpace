c3fe43fb5b8015ad088d35e34ab0842dae6044c5
yefko14 <61544277+yefko14@users.noreply.github.com>
2020-07-30 10:22:41 -0500

    running and aiming

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,26 +1,45 @@
-﻿using System.Collections;
+﻿using Cinemachine;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] Animator anim;
-    [SerializeField] float TurnSpeed = 15;
-    Camera mainCamera;
-
-    void Start()
-    {
-        mainCamera = Camera.main;
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+    [SerializeField] CinemachineFreeLook camara;
+    [SerializeField] float Aim = 15, normalView = 40;
 
     void Update()
     {
+        if (Input.GetMouseButton(1))
+        {
+            if (camara.m_Lens.FieldOfView > Aim)
+            {
+                camara.m_Lens.FieldOfView -= Time.time;
+            }
+            anim.SetBool("Aiming", true);
+            anim.SetBool("Running", false);
+        }
+        else if (Input.GetButton("Fire3"))
+        {
+            if (camara.m_Lens.FieldOfView < normalView)
+            {
+                camara.m_Lens.FieldOfView += Time.time;
+            }
+            anim.SetBool("Running", true);
+            anim.SetBool("Aiming", false);
+        }
+        else
+        {
+            if (camara.m_Lens.FieldOfView < normalView)
+            {
+                camara.m_Lens.FieldOfView += Time.time;
+            }
+            anim.SetBool("Running", false);
+            anim.SetBool("Aiming", false);
+        }
         anim.SetFloat("vertical", Input.GetAxis("Vertical"));
         anim.SetFloat("horizontal", Input.GetAxis("Horizontal"));
-
-        float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), TurnSpeed * Time.fixedDeltaTime);
     }
 }
