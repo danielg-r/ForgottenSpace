b3cdaa6ed15f8a7bf1e8616c127fb372948a8cfe
Aquafeniz <54065380+Aquafeniz@users.noreply.github.com>
2020-08-11 14:50:33 -0500

    Cinematic + Consumables + 50% Running Cooldown
    Se arreglaron un poco el flow de las cinematicas, haciendo que se esten cuadradas con el tiempo --- se hizo el script que permite recolectar los consumibles y usarlos ---- Se realizo el Cooldown de correr, para que no sea infinita ademas de acomodarla al consumible que la recupera mas rapida.

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,10 +6,18 @@ using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] Player player;
     [SerializeField] Animator anim;
     [SerializeField] CinemachineFreeLook camara;
     [SerializeField] float Aim = 15, normalView = 40;
 
+    public float stamina = 5f;
+    public float maxStamina = 5f;    
+
+    public void Start()
+    {
+        player = Player.Instance;
+    }
     void Update()
     {
         if (Input.GetMouseButton(1))
@@ -18,18 +26,28 @@ public class PlayerMovement : MonoBehaviour
             anim.SetBool("Aiming", true);
             anim.SetBool("Running", false);
         }
-        else if (Input.GetButton("Fire3"))
+        else if (Input.GetButton("Fire3") && stamina > 0f)
         {
             camara.m_Lens.FieldOfView = normalView;
             anim.SetBool("Running", true);
             anim.SetBool("Aiming", false);
+            stamina -= Time.deltaTime;
         }
         else
         {
+            if(stamina < maxStamina)
+            {
+                stamina += Time.deltaTime;
+            }
+            if (stamina < maxStamina && player.GetComponent<InventaryManager>().staminaBust == true)
+            {
+                stamina += 2*Time.deltaTime;
+            }
             camara.m_Lens.FieldOfView = normalView;
             anim.SetBool("Running", false);
             anim.SetBool("Aiming", false);
         }
+
         anim.SetFloat("vertical", Input.GetAxis("Vertical"));
         anim.SetFloat("horizontal", Input.GetAxis("Horizontal"));
     }
