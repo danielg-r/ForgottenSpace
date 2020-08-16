c07701667b9cd757771d3e9c726ccd95d292af9d
danielg-r <d4nielg.r@gmail.com>
2020-08-16 16:05:06 -0500

    Importadas un par de animaciones + CurrencyManager

--- Assets/Scripts/Consumables/Coin.cs
+++ Assets/Scripts/Consumables/Coin.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
-{    
-
-    [SerializeField] Player player;
-    float speed;
+{
+    [SerializeField] Player player; // = Player.Instance;
+    float speed = 8f;
     float distance;
 
     public bool Inter = false;
@@ -28,7 +27,6 @@ public class Coin : MonoBehaviour
                 //Destroy(this.gameObject);        
             } 
         }
-
     }
 
     void OnTriggerStay(Collider other)
@@ -37,13 +35,13 @@ public class Coin : MonoBehaviour
             {
                 other.GetComponent<InventaryManager>().RecolectCoin(1);
                 Destroy(this.gameObject);
-            } 
-        
+            }         
     }
 
     public void Atrract()
     {
         Inter = true;
+        
     }
 
 }
---
+++ Assets/Scripts/ScriptableObjects/CurrencyManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Currency Manager", menuName = "ForgottenSpace/CurrencyManager")]
+public class CurrencyManager : ScriptableObject
+{
+    public void AddCurrency(int amount)
+    {
+        //Instancia del inventario += amount
+        //Sonido (?)
+    }
+}
