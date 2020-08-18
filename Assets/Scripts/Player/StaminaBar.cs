3661e66a9b9c966b76c7bac556f3f753f1f33902
yefko14 <61544277+yefko14@users.noreply.github.com>
2020-08-18 08:32:40 -0500

    Barras Stamina, energía y animaciones mujer
    poseen bugs todavía ciertas cosas

---
+++ Assets/Scripts/Player/StaminaBar.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class StaminaBar : MonoBehaviour
+{
+    [SerializeField] Slider staminaBar;
+    [SerializeField] int waitToRegen = 2;
+    [SerializeField] int maxStamina = 100;
+    [SerializeField] int PlusRegen = 5;
+    [SerializeField] int waitToUse = 50;
+    int currentStamina;
+
+    WaitForSeconds regenTick = new WaitForSeconds(0.1f);
+    Coroutine regen;
+    public UnityEvent CantUse;
+    public UnityEvent CanUse;
+
+    bool WasCalled;
+
+    void Start()
+    {
+        currentStamina = maxStamina;
+        staminaBar.maxValue = maxStamina;
+        staminaBar.value = maxStamina;
+        WasCalled = false;
+    }
+
+    public void UseStamina(int amount)
+    {
+        if(currentStamina - amount >= 0)
+        {
+            currentStamina -= amount;
+            staminaBar.value = currentStamina;
+
+            if (regen != null)
+                StopCoroutine(regen);
+            regen = StartCoroutine(regenStamina());
+        }
+        else
+        {
+            CantUse.Invoke();
+            WasCalled = false;
+        }
+    }
+
+    IEnumerator regenStamina()
+    {
+        yield return new WaitForSeconds(waitToRegen);
+
+        while(currentStamina < maxStamina)
+        {
+            currentStamina += PlusRegen;
+            staminaBar.value = currentStamina;
+            yield return regenTick;
+            if (currentStamina > waitToUse && !WasCalled)
+            {
+                CanUse.Invoke();
+                WasCalled = true;
+            }
+        }
+        regen = null;
+    }
+}
