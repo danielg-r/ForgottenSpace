b5c6cc72c5be1a073a6653d11a971d596733a3cd
Thingy25 <68366292+Thingy25@users.noreply.github.com>
2020-08-21 22:11:18 -0500

    Añadidas piezas extra al inventario, importados algunos audios.

--- Assets/Scripts/Inventory/UIManager.cs
+++ Assets/Scripts/Inventory/UIManager.cs
@@ -7,16 +7,25 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI currencyText;
+    [SerializeField] private TextMeshProUGUI gunPieceText;
+    [SerializeField] private TextMeshProUGUI suitPieceText;
 
     private InventoryManager inventory;
+    
 
     void Start()
     {
+        if (CurrencyManager.Instance != null)
+        {
+            CurrencyManager.Instance.onCurrAdded += new CurrencyManager.OnCurrencyAdded(OnCurrAdded);
+        }
         inventory = InventoryManager.Instance;
     }
 
-    void Update()
+    void OnCurrAdded()
     {
         currencyText.text = inventory.currentCurrency.ToString();
+        gunPieceText.text = inventory.currentGunPieces.ToString();
+        suitPieceText.text = inventory.currentSuitPieces.ToString();
     }
 }
