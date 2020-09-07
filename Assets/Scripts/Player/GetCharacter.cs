90f2989b311d9ea92c92659b1cba2cabe0f5eb9d
yefko14 <61544277+yefko14@users.noreply.github.com>
2020-09-07 01:01:17 -0500

    selección de personaje y personaje mujer
    fix bug stamina y elección de personaje listo

---
+++ Assets/Scripts/Player/GetCharacter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetCharacter : MonoBehaviour
+{
+    readonly string selectedCharacter = "SelectedCharacter";
+    int getCharacter;
+    [SerializeField] GameObject[] skins;
+    Animator animator;
+
+    void Start()
+    {
+        animator = PlayerMovement.Instance.GetComponent<Animator>();
+        getCharacter = PlayerPrefs.GetInt(selectedCharacter);
+        // 1: hombre 2: mujer
+        switch (getCharacter)
+        {
+            case 1:
+                skins[0].SetActive(true);
+                skins[1].SetActive(false);
+                animator.runtimeAnimatorController = Resources.Load("Player") as RuntimeAnimatorController;
+                break;
+            case 2:
+                skins[0].SetActive(false);
+                skins[1].SetActive(true);
+                animator.runtimeAnimatorController = Resources.Load("PlayerWoman") as RuntimeAnimatorController;
+                break;
+        }
+    }
+}
