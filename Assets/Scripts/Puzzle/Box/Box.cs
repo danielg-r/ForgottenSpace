8ae4208e4c313f926c65df043768261ee09cdfbf
Aquafeniz <54065380+Aquafeniz@users.noreply.github.com>
2020-09-14 21:26:00 -0500

    PREFABS DEFINITIVOS

---
+++ Assets/Scripts/Puzzle/Box/Box.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class Box : MonoBehaviour
+{
+    public int index = 0;
+    int x = 0;
+    int y = 0;
+
+    private Action<int, int> swapFunc = null;
+
+    public void Init(int i, int j, int index, Sprite sprite, Action<int,int> swapFunc)
+    {
+        this.index = index;
+        this.GetComponent<SpriteRenderer>().sprite = sprite;
+        UpdatePos(i, j);
+        this.swapFunc = swapFunc;
+    }
+
+    public void UpdatePos(int i,int j)
+    {
+        x = i;
+        y = j;
+        this.gameObject.transform.localPosition = new Vector2(i, j);
+    }
+    IEnumerator Move()
+    {
+        float elapsedtime = 0f;
+        float duration = 0.2f;
+        Vector2 start = this.gameObject.transform.position;
+        Vector2 end = new Vector2(x, y);
+
+        while(elapsedtime<duration)
+        {
+            this.gameObject.transform.localPosition = Vector2.Lerp(start, end, (elapsedtime / duration));
+            elapsedtime += Time.deltaTime;
+            yield return null;
+        }
+
+        
+
+    }
+
+    public bool IsEmpty()
+    {
+        return index == 9;
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0) && swapFunc != null)
+        {
+           swapFunc(x,y);
+        }
+    }
+
+}
--- Assets/Scripts/UI/PauseSystem.cs
+++ Assets/Scripts/UI/PauseSystem.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseSystem : MonoBehaviour
 {
     public bool IsPaused;
     public GameObject PauseMenu;
+    public GameObject PauseButton;
+    public GameObject InstrucPanel;
+
+    public CursorManager cursorManager;
 
     private void Awake()
     {
@@ -16,27 +21,47 @@ public class PauseSystem : MonoBehaviour
     {
         if (IsPaused)
         {
-            //PauseMenu.SetActive(true);
+            PauseMenu.SetActive(true);
+            PauseButton.SetActive(false);
             Time.timeScale = 0f;
+            cursorManager.ShowCursor();
+
         }
         else
         {
-            //PauseMenu.SetActive(false);
+            PauseMenu.SetActive(false);
+            PauseButton.SetActive(true);
             Time.timeScale = 1f;
         }
-        if (Input.GetKeyDown(KeyCode.Escape))
+
+        if (Input.GetKeyDown(KeyCode.Q))
         {
             IsPaused = !IsPaused;
+            if(!IsPaused) cursorManager.HideCursor();
         }
     }
+
+    public void Exit()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ActivateInstruc()
+    {
+        InstrucPanel.SetActive(true);
+    }
+
+    public void CloseInstruc()
+    {
+        InstrucPanel.SetActive(false);
+    }
+
+
 
     public void Pause()
     {
         IsPaused = !IsPaused;
-    }
-    public void Exit()
-    {
-        Application.Quit();
+        cursorManager.HideCursor();
     }
 }
 
