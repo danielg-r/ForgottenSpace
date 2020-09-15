05991033399f9906b114aa64a3a1dd1750d477e9
danielg-r <d4nielg.r@gmail.com>
2020-09-14 19:55:14 -0500

    Finish Sprint 7
    - Implementado nuevo sistema de detección + cámaras + cambiada la detección en los dron patrulla.
    - Aplicado el buff a los robots para tener una sobrecarga cuando mueren y explotar para hacer daño al jugador.

---
+++ Assets/Scripts/Utility/Viewcone/ViewconeDetection.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+
+public class ViewconeDetection : MonoBehaviour {
+
+	//public GameObject AlertIcon;
+	private const string ObjectTag = "Player"; // Change if you are checking for another tag.
+	public Transform Character;			// Transform of the character.
+	public UnityEvent OnDetected;  //Event that gets called when the tag is detected.
+
+	void Start () 
+	{
+		//AlertIcon.SetActive (false);
+		if (Character == null)
+		{
+			Debug.LogError (ObjectTag + " viewcone character property is not set!");
+		}
+	}
+
+	public void ObjectSpotted (Collider col) 
+	{
+		if(col.CompareTag(ObjectTag))
+		{
+			RaycastHit newHit;
+			Debug.DrawRay(transform.position, col.transform.position - transform.position);
+
+			if(Physics.Raycast (new Ray(transform.position, col.transform.position - transform.position), out newHit))
+			{
+				if(newHit.collider.CompareTag(ObjectTag))
+				{
+					Debug.LogWarning (ObjectTag + " spotted by " + Character.name + ".");
+					OnDetected.Invoke();
+					//AlertIcon.SetActive (true);
+
+				}
+				else
+				{
+					Debug.Log (ObjectTag + " within viewcone of " + Character.name + ", but is obstructed by" + newHit.collider.name);
+					//AlertIcon.SetActive (false);
+
+				}
+			}
+		}
+	}
+
+	public void ObjectLeft (Collider col)
+	{
+		if(col.CompareTag(ObjectTag))
+		{
+			//AlertIcon.SetActive (false);
+		}
+
+	}
+}
