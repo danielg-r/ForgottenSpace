ff51be350bb2fea147629fb7d2f30af42856ca2b
Thingy25 <68366292+Thingy25@users.noreply.github.com>
2020-10-15 18:15:35 -0500

    Sonidos caminata en progreso, lore modificado :3

---
+++ Assets/Scripts/Misc/AudioEvents.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioEvents : MonoBehaviour
+{
+   public void Walk1()
+   {
+        AudioManager.Instance.Play("Walk1");
+   }
+
+   public void Walk2()
+   {
+        AudioManager.Instance.Play("Walk2");
+   }
+}
