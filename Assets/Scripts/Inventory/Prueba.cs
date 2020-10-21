e84597db185e1f9f37cd1b53d73d0170b93e4076
Thingy25 <68366292+Thingy25@users.noreply.github.com>
2020-10-21 02:15:18 -0500

    Algunos audios comprimidos + creado prefab easter egg

---
+++ Assets/Scripts/Inventory/Prueba.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Prueba : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.H))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        } 
+    }
+    public void ChangeVolume(float vol)
+    {
+        mixer.SetFloat("Volume", vol);
+    }
+}
