3c58631eca00cb1fc15755669fdf0f0544c1d3cf
Thingy25 <68366292+Thingy25@users.noreply.github.com>
2020-10-30 04:01:53 -0500

    Adivina que? Ahora puedes inspeccionar el lore desde el inventario

--- Assets/Scripts/Inventory/LoreInspect.cs
+++ Assets/Scripts/Inventory/LoreInspect.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Michsky.UI.ModernUIPack;
 
 public class LoreInspect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //Pasarle un modal window al item y guardarlos en un array de las mismas en el InventoryManager, mandarle un id al método de este script
+    //
+    //Y colocarselo al botón del slot en el inventario, y ahí pasarle el ID correspondiente a la posición del array
+    //
+    //jeje
+    private Slot slot;
 
-    // Update is called once per frame
-    void Update()
+    public void InspectEntry (int id)
     {
-        
+        for (int i = 0; i < InventoryManager.Instance.loreSlots.Length; i++)
+        {
+            if (InventoryManager.Instance.loreSlots[id].GetComponent<Slot>().isEmpty == false)
+            {
+                InventoryManager.Instance.windowArray[id].OpenWindow();
+                CursorManager.Instance.ShowCursor();
+                Inventory.Instance.canClose = false;
+            }        
+        }      
     }
 }
---
+++ Assets/Scripts/Misc/LoreCursor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoreCursor : MonoBehaviour
+{
+    public void ShowCursor()
+    {
+         Time.timeScale = 0f;
+         Cursor.lockState = CursorLockMode.None;
+         Cursor.visible = true;       
+    }
+
+    public void HideCursor()
+    {
+        if (Inventory.Instance.isOpen == false)
+        {
+            Time.timeScale = 1f;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else { Inventory.Instance.canClose = true; }
+    }
+}
